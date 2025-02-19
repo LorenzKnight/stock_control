0f0262380f46340ac19f31617e54f055dc14c636
“lknight” <lorenz.knight@impactct.com>
2025-02-19 23:24:42 +0100

    fixed page recharge botton  bug

--- public/js/actions.js
+++ public/js/actions.js
@@ -515,44 +515,46 @@ document.addEventListener("DOMContentLoaded", () => {
 		});
 	});
 
-	let albumDislike = document.querySelectorAll('.album-dislike');
+	document.addEventListener('click', function(e) {
+		console.log(e);
+		if (!e.target.classList.contains('album-dislike')) { 
+			return
+		}
+		
+		let element = e.target 
+		let albumId = element.getAttribute('data-albumId');
 
-	albumDislike.forEach(function(element){ // falta crear un refresh 
-		element.addEventListener('click', function(){
-			let albumId = element.getAttribute('data-albumId');
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var response = JSON.parse(this.responseText);
+				console.log(response);
 
-			var xmlhttp = new XMLHttpRequest();
-			xmlhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-					var response = JSON.parse(this.responseText);
-					console.log(response);
+				const statusMessageElem = document.getElementById('status-message');
+				if (statusMessageElem) {
+					statusMessageElem.innerText = response.message;
+					statusMessageElem.style.display = 'block';
 
-					const statusMessageElem = document.getElementById('status-message');
-					if (statusMessageElem) {
-						statusMessageElem.innerText = response.message;
-						statusMessageElem.style.display = 'block';
-	
+					setTimeout(() => {
+						statusMessageElem.style.opacity = '0';
 						setTimeout(() => {
-							statusMessageElem.style.opacity = '0';
-							setTimeout(() => {
-								statusMessageElem.style.display = 'none';
-								statusMessageElem.style.opacity = '0.9';
-							}, 1000);
-						}, 2000);
-					}
+							statusMessageElem.style.display = 'none';
+							statusMessageElem.style.opacity = '0.9';
+						}, 1000);
+					}, 2000);
+				}
 
-					if (response.success && response.html) {
-						document.getElementById('list-content').innerHTML = response.html;
-					}
+				if (response.success && response.html) {
+					document.getElementById('list-content').innerHTML = response.html;
 				}
 			}
-			var formData = new FormData(); 
-			formData.append('MM_insert', 'removeFromFav');
-			formData.append('albumId', albumId);
-		
-			xmlhttp.open("POST", "logic/discover_be.php", true);
-			xmlhttp.send(formData);
-		});
+		}
+		var formData = new FormData(); 
+		formData.append('MM_insert', 'removeFromFav');
+		formData.append('albumId', albumId);
+	
+		xmlhttp.open("POST", "logic/discover_be.php", true);
+		xmlhttp.send(formData);
 	});
 
 });
