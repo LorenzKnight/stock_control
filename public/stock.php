3370703f0cc6654c1a7e43e6d7122912a8475bbf
“lknight” <lorenz.knight@impactct.com>
2025-02-26 15:31:10 +0100

    stock page

---
+++ public/stock.php
@@ -0,0 +1,21 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Stock Control</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- <link rel="stylesheet" href="css/components.css"> -->
+    <script src="https://kit.fontawesome.com/5e05ee9535.js" crossorigin="anonymous"></script> <!-- iconos del reproductor -->
+    <!-- <script defer src="js/music_player.js"></script>
+	<script defer src="js/actions.js"></script>
+	<script defer src="js/uploader.js"></script> -->
+</head>
+
+<body>
+    <div class="container" id="result-container">
+        aqui
+    </div>
+</body>
+
+</html>
