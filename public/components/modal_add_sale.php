d0710db6cb09bb43da6507040a06547ba5d0c509
“lknight” <lorenz.knight@impactct.com>
2025-05-01 12:48:06 +0200

    optimize sales searches

---
+++ public/components/modal_add_sale.php
@@ -0,0 +1,99 @@
+<div class="bg-popup" id="add-sale-form">
+	<div class="formular-big-frame">
+        <form method="post" name="formAddCategory" id="formAddCategory">
+            <table width="80%" align="center" cellspacing="0">
+				<tr valign="baseline">
+					<td colspan="6" align="center" valign="middle">
+						<h2>Create a Sale</h2>
+					</td>      
+				</tr>
+				<tr valign="baseline">
+					<td colspan="2" align="center" valign="middle">
+						<div class="formular-category-list">
+							<div class="create-list-holder" id="clic-create-mark">
+								<button type="button" class="button-style-agree" id="add-mark-btn">New Mark</button>
+							</div>
+							<div class="create-list-holder flex" id="input-mark" style="display: none;">
+								<table width="100%" align="center" cellspacing="0">
+									<tr valign="baseline">
+										<td width="70%" align="center" valign="middle">
+											<input type="text" class="form-medium-input-style" name="input-product-mark" id="input-product-mark" />
+										</td>
+										<td width="30%" align="center" valign="middle">
+											<button type="button" class="button-style-agree" id="btn-create-mark">Create</button>
+										</td>
+									</tr>
+								</table>
+							</div>
+							<div class="cat-all-list">
+								<table class="all-mark-list" id="mark-list" cellspacing="0">
+								
+								</table>
+							</div>
+						</div>
+					</td>
+					<td colspan="2" align="center" valign="middle">
+						<div class="formular-category-list">
+							<div class="create-list-holder" id="clic-create-model">
+								<button type="button" class="button-style-agree disabled" id="add-model-btn" disabled>New Model</button>
+							</div>
+							<div class="create-list-holder" id="input-model" style="display: none;">
+								<table width="100%" align="center" cellspacing="0">
+									<tr valign="baseline">
+										<td width="70%" align="center" valign="middle">
+											<input type="text" class="form-medium-input-style" name="input-product-model" id="input-product-model" />
+										</td>
+										<td width="30%" align="center" valign="middle">
+											<button type="button" class="button-style-agree" id="btn-create-model">Create</button>
+										</td>
+									</tr>
+								</table>
+							</div>
+							<div class="cat-all-list">
+								<table class="all-model-list" id="model-list" cellspacing="0">
+								
+								</table>
+							</div>
+						</div>
+					</td>
+					<td colspan="2" align="center" valign="middle">
+						<div class="formular-category-list">
+							<div class="create-list-holder" id="clic-create-submodel">
+								<button type="button" class="button-style-agree disabled" id="add-submodel-btn" disabled>New Submodel</button>
+							</div>
+							<div class="create-list-holder" id="input-submodel" style="display: none;">
+								<table width="100%" align="center" cellspacing="0">
+									<tr valign="baseline">
+										<td width="70%" align="center" valign="middle">
+											<input type="text" class="form-medium-input-style" name="input-product-submodel" id="input-product-submodel" />
+										</td>
+										<td width="30%" align="center" valign="middle">
+											<button type="button" class="button-style-agree" id="btn-create-submodel">Create</button>
+										</td>
+									</tr>
+								</table>
+							</div>
+							<div class="cat-all-list">
+								<table class="all-submodel-list" id="submodel-list" cellspacing="0">
+								
+								</table>
+							</div>
+						</div>
+					</td>
+				</tr>
+				<tr valign="baseline" class="form_height">
+					<td colspan="2" align="center" valign="middle">
+					</td>
+					<td colspan="1" align="center" valign="middle">
+						<button type="button" class="neutral-btn">Cancel</button>
+					</td>
+					<td colspan="1" align="center" valign="middle">
+						<input type="submit" class="button-style-agree" value="Create" />
+					</td>
+					<td colspan="2" align="center" valign="middle">
+					</td>
+				</tr>
+			</table>
+        </form>
+    </div>
+</div>
--- public/components/sales_container.php
+++ public/components/sales_container.php
@@ -4,7 +4,7 @@
 			<table width="100%" align="center" cellspacing="0">
 				<tr valign="baseline">
 					<td width="15%" align="center" valign="middle">
-						<button type="button" class="button-style-agree" id="add-customers-button">Create Sale</button>
+						<button type="button" class="button-style-agree" id="add-sale-btn">Create Sale</button>
 					</td>
 					<td width="85%" align="center" valign="middle">
 						<input type="text" name="salesSearchField" id="salesSearchField" class="big-search-field" placeholder="Search Sale..." title="Search Sale">
@@ -16,114 +16,7 @@
 	<div class="data-container" style="height: 710px;">
 		<h2 style="margin-left: 10px;">Sales List</h2>
 		<div class="centralize" style="width: 100%;">
-			<div class="sales-list" id="sales-list">
-
-				<!-- <div class="sale-row">
-					<table width="100%" style="border-bottom: 1px solid #999;" align="center" cellspacing="0">
-						<tr valign="baseline" class="form_height">
-							<td width="10%" align="left" valign="middle">
-							<p class="mini-title">Ord. No:</p>
-								123-123 123
-							</td>
-							<td width="87%" align="center" valign="middle">
-							</td>
-							<td width="3%" align="center" valign="middle">
-								<div class="sale-menu">
-									<img src="images/sys-img/hamburger-menu-icon.png" alt="menu">
-								</div>
-							</td>
-						</tr>
-					</table>
-					<div class="flex" style="width: 100%; margin-top: 5px;">
-						<table width="30%" align="center" cellspacing="0">
-							<tr valign="baseline" class="form_height">
-								<td width="30%" align="left" valign="middle">
-									<div class="sale-profile">
-										<img src="images/sys-img/NonProfilePic.png" alt="profile picture">
-									</div>
-								</td>
-								<td width="70%" align="left" valign="middle">
-									<h3><strong>Nombre Apellido</strong></h3>
-
-									<p class="mini-title">Cedula:</p>
-									123 123 1234
-									</br></br>
-
-									<p class="mini-title">Phone:</p>
-									+46 71 231 2454
-								</td>
-							</tr>
-						</table>
-						<table width="40%" style="border-left: 1px solid #999;" align="center" cellspacing="0">
-							<tr valign="baseline" class="form_height">
-								<td width="30%" align="left" valign="middle">
-									<div class="sale-product-pic">
-										<img src="images/sys-img/wooden-box.png" alt="product picture">
-									</div>
-								</td>
-								<td width="80%" align="left" valign="middle">
-									<p class="mini-title">Product No:</p>
-									A86986876576
-									
-									<h3><strong>Mercedez Benz - Class A</strong></h3>
-									<p>A 200 Diesel 4 Matic</p>
-
-									<p class="mini-title">Year</p>
-									<strong>2017</strong>
-								</td>
-							</tr>
-						</table>
-						<table width="30%" style="border-left: 1px solid #999;" align="center" cellspacing="0"> -->
-							<!-- <tr valign="baseline" class="form_height">
-								<td>
-									Method of Payment<br>
-
-									precio<br>
-									inicial<br>
-									fecha de entrega<br> 
-
-									restante<br>
-									interes<br>
-									plan de pago, cuotas por mes<br>
-									fecha de pago<br>
-									faltante por pagar<br>
-								</td>
-							</tr> -->
-							<!-- <tr valign="baseline" class="form_height">
-								<td colspan="2" style="padding-left: 7px;" align="left" valign="middle"><strong>Method of Payment</strong></td>
-							</tr>
-							<tr valign="baseline" class="form_height">
-								<td width="35%" align="right" valign="middle">Price :</td><td width="65%" style="padding-left: 5px;" align="left" valign="middle">500 000</td>
-							</tr>
-							<tr valign="baseline" class="form_height">
-								<td width="35%" align="right" valign="middle">Initial :</td><td width="65%" style="padding-left: 5px;" align="left" valign="middle">100 000</td>
-							</tr>
-							<tr valign="baseline" class="form_height">
-								<td width="35%" align="right" valign="middle">Delivery date :</td><td width="65%" style="padding-left: 5px;" align="left" valign="middle">2024-05-10</td>
-							</tr>
-							<tr valign="baseline" class="form_height">
-								<td width="35%" align="right" valign="middle">Remaining :</td><td width="65%" style="padding-left: 5px;" align="left" valign="middle">400 000</td>
-							</tr>
-							<tr valign="baseline" class="form_height">
-								<td width="35%" align="right" valign="middle">Interest :</td><td width="65%" style="padding-left: 5px;" align="left" valign="middle">400 000 + 10% = 440 000</td>
-							</tr>
-							<tr valign="baseline" class="form_height">
-								<td width="35%" align="right" valign="middle">Installments / month :</td><td width="65%" style="padding-left: 5px;" align="left" valign="middle">7 333.33</td>
-							</tr>
-							<tr valign="baseline" class="form_height">
-								<td width="35%" align="right" valign="middle">No. of installments :</td><td width="65%" style="padding-left: 5px;" align="left" valign="middle">5 / 60</td>
-							</tr>
-							<tr valign="baseline" class="form_height">
-								<td width="35%" align="right" valign="middle">Payment date :</td><td width="65%" style="padding-left: 5px;" align="left" valign="middle">Every 27th</td>
-							</tr>
-							<tr valign="baseline" class="form_height">
-								<td width="35%" align="right" valign="middle">Due :</td><td width="65%" style="padding-left: 5px;" align="left" valign="middle">403 333.35</td>
-							</tr>
-						</table>
-					</div>
-				</div> -->
-
-			</div>
+			<div class="sales-list" id="sales-list"></div>
 		</div>
 	</div>
 </div>
