1c361a118229e1cba9f2d09733280f49cf669fb9
Lorenzo Knight <joellorenzo.k@gmail.com>
2025-09-02 13:37:07 +0200

    fix mobile floating header

--- public/components/front_header.php
+++ public/components/front_header.php
@@ -10,10 +10,20 @@
         <li class="no-redirect"><a href="" id="toggle-link">Sign up</a></li>
     </ul>
 
-	<div class="mobile-logo">
-		<img src="images/sys-img/asc-logo.png" alt="Logo" class="asc-logo-header">
+	<div class="mobile-menu-container">
+		<div class="mobile-logo">
+			<img src="images/sys-img/asc-logo.png" alt="Logo" class="asc-logo-header">
+		</div>
+		<ul class="menu-hamburger" style="background-color: transparent; justify-content: flex-end; align-items: center; padding-right: 0px;">
+			<li class="no-redirect"><a href="" id="home-btn-mobile"><img src="images/sys-img/hamburger-menu-icon.png"></a></li>
+		</ul>
 	</div>
-	<ul class="menu-mobile">
-		<li class="no-redirect"><a href="" id="home-btn-mobile"><img src="images/sys-img/hamburger-menu-icon.png"></a></li>
-	</ul>
+	<div class="mobile-menu" id="mobile-menu">
+		<ul class="mobile-menu-list">
+			<li class="no-redirect"><a href="" id="home-btn-mobile-2">Home</a></li>
+			<li class="no-redirect"><a href="" id="features-btn-mobile">Features</a></li>
+			<li class="no-redirect"><a href="" id="pricing-btn-mobile">Pricing</a></li>
+			<li class="no-redirect"><a href="" id="toggle-link-mobile">Sign up</a></li>
+		</ul>
+	</div>
 </div>
