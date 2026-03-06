5c23da90c800fa8a0ad4b1a0868047ed21409403
Lorenzo Knight <joellorenzo.k@gmail.com>
2026-03-06 12:29:57 +0100

    implement agree terms and gdpr

--- public/components/message.php
+++ public/components/message.php
@@ -5,8 +5,8 @@
 
 <div id="globalConfirmModal" class="delete-modal-overlay" style="display: none;">
 	<div class="delete-modal-box">
-		<h3 id="confirm-modal-title">Confirm Action</h3>
-		<p id="confirm-modal-message">Are you sure you want to proceed?</p>
+		<h3 id="confirm-modal-title"></h3>
+		<p id="confirm-modal-message"></p>
 		<div class="delete-modal-actions">
 			<button id="modalCancelBtn" class="neutral-btn">Cancel</button>
 			<button id="modalConfirmBtn" class="button-style-agree">Confirm</button>
@@ -16,8 +16,8 @@
 
 <div id="globalOkModal" class="delete-modal-overlay" style="display: none;">
 	<div class="delete-modal-box">
-		<h3 id="alert-modal-title">Confirm Action</h3>
-		<p id="alert-modal-message">Are you sure you want to proceed?</p>
+		<h3 id="alert-modal-title"></h3>
+		<p id="alert-modal-message"></p>
 		<div class="confirm-modal-actions">
 			<button id="modalOkBtn" class="button-style-agree">Ok</button>
 		</div>
